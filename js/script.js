eb10771a09c0d5a09315d03cacca79e634df62b4
edysantoso54321 <42992533+edysantoso54321@users.noreply.github.com>
2019-04-20 01:08:06 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var dr=2; var lr=0.1; var drp=2; var lrp=2; var neuron=[];var jumneuron=14;var data=[]; var np=7;
+var dr=2; var lr=0.1; var drp=5; var lrp=750; var neuron=[];var jumneuron=14;var data=[]; var np=7;
 function dist(a,b){return Math.sqrt(Math.pow((a[0]-b[0]),2)+Math.pow((a[1]-b[1]),2));}
 function fidt(a,b){return Math.exp((-1*Math.pow(dist(a,b),2))/(2*Math.pow(dr,2)));}
 function delw(a,b,c,d){return lr*fidt(a,b)*(c[d]-b[d]);}
